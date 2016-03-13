40486625811684d46cac0923cf9719a1b537e94a
Kestred <kestred@riotcave.com>
2016-03-12 16:35:24 -0800

    Reduce noise in assertion stacktrace

--- code/utils/assert.go
+++ code/utils/assert.go
@@ -5,17 +5,14 @@ import (
 	"reflect"
 	"runtime"
 	"strings"
+	"os"
 )
-
-type FailedAssertion string
-
-func (fa FailedAssertion) Error() string {
-	return string(fa)
-}
 
 func Assert(truthy bool, msg string) {
 	if !truthy {
-		panic(FailedAssertion(msg))
+		fmt.Println("Assertion:", msg)
+		printStack()
+		os.Exit(1)
 	}
 }
 
@@ -25,7 +22,9 @@ func AssertNil(value interface{}, msg string) {
 	//   IsValid returns true unless called on the zero Value
 	//
 	if reflect.ValueOf(value).IsValid() {
-		panic(FailedAssertion(fmt.Sprintf("%v (%v != nil)", msg, value)))
+		fmt.Printf("Assertion: %v (%v != nil)\n", msg, value)
+		printStack()
+		os.Exit(1)
 	}
 }
 
@@ -33,6 +32,31 @@ func InvalidCodePath() {
 	pc, _, line, _ := runtime.Caller(1)
 	path := strings.Split(runtime.FuncForPC(pc).Name(), "/")
 	caller := path[len(path)-1]
-	message := fmt.Sprintf("Reached invalid code path at %v:%v\n", caller, line)
-	panic(FailedAssertion(message))
+
+	fmt.Printf("Assertion: Reached invalid code path at %v:%v\n", caller, line)
+	printStack()
+	os.Exit(1)
+}
+
+func printStack() {
+	fmt.Println()
+	for i := 2; i < 50; i++ {
+		pc, file, line, ok := runtime.Caller(i)
+		if !ok {
+			fmt.Println()
+			return
+		}
+
+		pathParts := strings.Split(file, "/")
+		path := pathParts[len(pathParts)-1]
+		if len(path) > 16 {
+			path = "(*)" + path[len(path)-14:len(path)]
+		}
+
+		callerParts := strings.Split(runtime.FuncForPC(pc).Name(), "/")
+		caller := callerParts[len(callerParts)-1]
+		fmt.Printf("  %16s  %s:%d\n", path, caller, line)
+	}
+
+	fmt.Println("Why things fail: your stack is to deep for sanity...")
 }
